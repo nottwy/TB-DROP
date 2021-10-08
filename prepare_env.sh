f895ce5b205ea15828d24ff03e9d07cab6896f6c
Constantine-HMC <89449410+Constantine-HMC@users.noreply.github.com>
2021-10-08 22:01:06 +0800

    prepare_env.sh

--- prepare_env.sh
+++ prepare_env.sh
@@ -3,3 +3,4 @@ usermod -d /var/lib/mysql mysql
 service mysql start
 mysql -uroot < /root/pipeline/tb-visualization/01.software/create.sql
 nohup python3 /root/pipeline/tb-visualization/03.server/flask/run.py &> /dev/null &
+
