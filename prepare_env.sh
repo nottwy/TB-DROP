062bf5696c2d3d15afee26f63f9048564da690b1
Constantine-HMC <89449410+Constantine-HMC@users.noreply.github.com>
2021-10-08 22:33:04 +0800

    Delete prepare_env.sh

--- prepare_env.sh
+++
@@ -1,6 +0,0 @@
-tar -zxvf pipeline.tar.gz 
-usermod -d /var/lib/mysql mysql
-service mysql start
-mysql -uroot < /root/pipeline/tb-visualization/01.software/create.sql
-nohup python3 /root/pipeline/tb-visualization/03.server/flask/run.py &> /dev/null &
-
